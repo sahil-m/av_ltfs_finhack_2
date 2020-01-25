1aa98679393789b5f4e80180c2e85268512f488d
in-rmoitra <rmoitra@thoughtworks.com>
2020-01-25 19:45:04 +0530

    Add holidays to ts visualisations for EDA

--- eda_sahil_2.R
+++ eda_sahil_2.R
@@ -4,7 +4,7 @@ segment1_date_level <- read_csv('data/segment1_date_level.csv')
 segment2_date_level <- read_csv('data/segment2_date_level.csv')
 
 holidays <- read_csv('data/holidays_cleaned.csv') %>% 
-  filter(as.Date(Date) > min(segment1_date_level$application_date) & Date < max(segment1_date_level$application_date))
+  filter(date > min(segment1_date_level$application_date) & date < max(segment1_date_level$application_date))
 
 ##### visualizing
 # segment1_date_level_xts <- xts(dplyr::select(segment1_date_level, case_count), order.by = segment1_date_level$application_date)
@@ -23,7 +23,7 @@ ggplotly(
   segment1_date_level %>%
     ggplot(aes(x = application_date, y = case_count)) +
     geom_line() + 
-    geom_vline(xintercept= as.numeric(holidays$Date), linetype=4, color='red') +
+    geom_vline(xintercept= as.numeric(holidays$date), linetype=4, color='red') +
     geom_point(aes(size = I(.5))) +
     geom_area(aes(y=is_weekend*y_limit), fill="yellow", alpha = .3) +
     # stat_smooth(method = "loess", aes(color = branch_id, fill = branch_id)) +
@@ -46,6 +46,22 @@ ggplotly(
   tooltip = c('label', 'y')
 )
 
+
+## For segment 2 ##
+y_limit <- max(segment2_date_level$case_count)
+ggplotly(
+  segment2_date_level %>%
+    ggplot(aes(x = application_date, y = case_count)) +
+    geom_line() + 
+    geom_vline(xintercept= as.numeric(holidays$date), linetype=4, color='red') +
+    geom_point(aes(size = I(.5))) +
+    geom_area(aes(y=is_weekend*y_limit), fill="yellow", alpha = .3) +
+    # stat_smooth(method = "loess", aes(color = branch_id, fill = branch_id)) +
+    scale_y_continuous(limits = c(0, y_limit)) +
+    scale_x_date(date_labels = "%b/%Y", date_breaks = "1 month") +
+    theme(axis.text.x = element_text(angle=45, hjust = 1))
+)
+
 ggplotly(
   segment2_date_level %>%
     ggplot(aes(x = day_of_year, y = case_count, label = label)) +
@@ -57,3 +73,5 @@ ggplotly(
     theme_bw(),
   tooltip = c('label', 'y')
 )
+
+
