aa8e8127afc69f577e3b8be6ba1c530dfd34825e
in-rmoitra <rmoitra@thoughtworks.com>
2020-01-26 15:48:07 +0530

    add test predictions for decomposition h2o script

--- decomposition_h2o_s2.R
+++ decomposition_h2o_s2.R
@@ -4,11 +4,11 @@ library('h2o')
 
 set.seed(123L)
 
-#### Read date level data
+#### Read date level data ####
 load('data/segment_wise_date_level_msts_objects.Rdata')
 load('data/segment_wise_date_level_splitted_objects.Rdata')
 
-#### just 2019 - STR decomposition
+#### just 2019 - STR decomposition ####
 # s2_train_msts %>%
 #   AutoSTR() %>%
 #   plot()
@@ -148,12 +148,117 @@ validation_s2_for_model$ape <- errors_df$ape
 summary(errors_df$ape)
 
 #### Generate test predictions ####
+source('./utils.R')
+library('stR')
+library('h2o')
 
+set.seed(123L)
+
+#### Read date level data ####
+load('data/segment_wise_date_level_msts_objects.Rdata')
+load('data/segment_wise_date_level_splitted_objects.Rdata')
+
+s2_test <- read_csv('data/test.csv') %>% 
+  filter(segment == 2)
+
+segment_2_date_level <- read_csv('data/segment2_date_level.csv')
 test_data_s2 <- read_csv('data/test.csv') %>% filter(segment == 2)
 test_data_s2 <- add_date_based_features(test_data_s2)
 
 test_data_s2 <- test_data_s2 %>% 
-  select(is_end_of_month, is_weekend, day_of_month, trend_mean)
+  select(application_date, is_end_of_month, is_weekend, day_of_month, day_of_week, year) %>% 
+  mutate('day_of_week' = as.character(day_of_week),
+         'week_of_month_2' = as.factor(ceiling(day_of_month / 7)),
+         'is_sunday' = day_of_week == 'Sun')
+
+##### feature prep.
+s2_str_object <- AutoSTR(segment2_date_level_msts, robust = FALSE)
+
+s2_str_features <- segment_2_date_level %>% 
+  dplyr::select(application_date,
+                case_count,
+                is_end_of_month,
+                is_weekend,
+                day_of_month,
+                day_of_week,
+                year) %>% 
+  mutate('week_of_month_2' = as.factor(ceiling(day_of_month / 7)),
+         'is_sunday' = day_of_week == 'Sun') %>% 
+  mutate(trend = s2_str_object$output$predictors[[1]]$data,
+         season_7 = s2_str_object$output$predictors[[2]]$data,
+         season_30 = s2_str_object$output$predictors[[3]]$data) %>% 
+  bind_rows(dplyr::select(test_data_s2, application_date, day_of_week, is_end_of_month, is_weekend, week_of_month_2, is_sunday, day_of_month, year)) %>% 
+  group_by(year) %>% 
+  mutate(trend_mean = mean(trend, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  group_by(year, day_of_week) %>% 
+  mutate(season_7_mean_by_wday = mean(season_7, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  group_by(year, day_of_month) %>% 
+  mutate(season_30_mean_by_mday = mean(season_30, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  select(-trend, -season_7, -season_7_mean_by_wday, -season_30, -day_of_week, -year)
+
+validaton_s2_start_date <- ymd("2019-04-23")
+test_s2_start_date <- min(test_data_s2$application_date)
+
+train_plus_valid_s2_for_model <- s2_str_features %>% 
+  filter(application_date < test_s2_start_date) %>% 
+  left_join(select(segment_2_date_level, application_date, case_count)) %>% 
+  select_if(~ !is.Date(.)) 
+
+test_s2_for_model <- s2_str_features %>% 
+  filter(application_date >= test_s2_start_date) %>% 
+  select_if(~ !is.Date(.)) %>% 
+  select(-case_count)
+
+validation_s2_for_model <- s2_str_features %>% 
+  filter((application_date >= validaton_s2_start_date) & (application_date < test_s2_start_date)) %>% 
+  left_join(select(s2_validation_data, application_date, case_count)) %>% 
+  select_if(~ !is.Date(.)) 
+
+#### h20
+h2o.init()
+
+train_plus_valid_h2o <- as.h2o(train_plus_valid_s2_for_model)
+valid_h2o <- as.h2o(validation_s2_for_model)
+test_h2o  <- as.h2o(test_s2_for_model)
+
+y <- "case_count"
+x <- setdiff(names(train_plus_valid_h2o), c(y, 'index.num', 'label'))
+
+automl_models_h2o <- h2o.automl(
+  x = x, 
+  y = y, 
+  training_frame = train_plus_valid_h2o, 
+  max_runtime_secs = 0,
+  exclude_algos = c("DeepLearning", "StackedEnsemble"),
+  seed = 123)
+
+automl_leader <- automl_models_h2o@leader
+
+h2o.varimp(automl_leader)
+
 pred_h2o <- h2o.predict(automl_leader, newdata = valid_h2o)
+errors_df <- getAPE(validation_s2_for_model$case_count, pred_h2o %>% as_tibble() %>% pull(predict))
+summary(errors_df$ape)
+
+pred_h2o <- h2o.predict(automl_leader, newdata = test_h2o)
+
+test_submission <- s2_test %>% 
+  add_column(pred = pred_h2o %>% as_tibble() %>% pull(predict)) %>% 
+  rename(case_count = pred)
+
+pred_on_train_s2_object %>%
+  autoplot() +
+  geom_line(data = s2_validation_msts,
+            aes(
+              x = as.numeric(time(s2_validation_msts)),
+              y = as.numeric(s2_validation_msts)
+            ),
+            col = "red"
+  )
+
+write_csv(test_submission, 'data/predictions/segment_2_h2o.csv')
 
 
